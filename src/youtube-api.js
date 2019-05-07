3568010ea882e13aea00eeacbc763c68bf19a664
Sami BUrack <samiburack@gmail.com>
2019-05-07 15:02:40 -0400

    SA done

--- src/youtube-api.js
+++ src/youtube-api.js
@@ -1,24 +1,53 @@
+/* eslint-disable indent */
 import axios from 'axios';
+import { each } from 'async';
 
 const API_URL = 'https://www.googleapis.com/youtube/v3/search';
-const API_KEY = 'YOUR_KEY';
+const API_KEY = 'AIzaSyBFW-Vj-r2M0CsZPvK4URD8u8fo-Qh9lZw';
+const STATISTICS_API_URL = 'https://www.googleapis.com/youtube/v3/videos';
 
+export const viewCountByVideo = (videoId) => {
+  const params = {
+    key: API_KEY,
+    id: videoId,
+    part: 'statistics',
+  };
+  return new Promise((resolve, reject) => {
+    axios.get(STATISTICS_API_URL, { params })
+      .then((response) => {
+        resolve(Number(response.data.items[0].statistics.viewCount));
+      })
+      .catch((error) => {
+        console.log(`youtube statistics api error: ${error}`);
+        reject(error);
+      });
+  });
+};
+
+// eslint-disable-next-line import/prefer-default-export
 export const youtubeSearch = (term) => {
   const params = {
-    part: 'snippet',
     key: API_KEY,
     q: term,
     type: 'video',
+    part: 'snippet',
   };
-
   return new Promise((resolve, reject) => {
     axios.get(API_URL, { params })
       .then((response) => {
-        resolve(response.data.items);
-      })
-      .catch((error) => {
-        console.log(`youtube api error: ${error}`);
-        reject(error);
+        let totalViewCounter = 0;
+        each(response.data.items, (item, callback) => {
+          viewCountByVideo(item.id.videoId).then((views) => {
+            totalViewCounter += Number(views);
+            callback();
+          });
+        },
+          (err) => {
+            resolve({
+              all: response.data.items,
+              totalViews: totalViewCounter,
+            });
+          });
       });
   });
 };
